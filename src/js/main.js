87af44261ae3d9e314fad3ba9d31a9c12efa8e33
daanlouter <daan.louter@gmail.com>
2015-07-06 15:06:05 +0100

    mute button

--- src/js/main.js
+++ src/js/main.js
@@ -7,20 +7,29 @@ define([
 ) {
     'use strict';
 
-    function handleRequestError(err, msg) {
-        console.error('Failed: ', err, msg);
-    }
-    function afterRequest(resp) {
-        // console.log('Finished', resp);
-    }
-
     function init(el) {
+        var mute =  false;
         @@template@@
+        console.log('hey')
         $(".content--interactive").html(template["index.html"]);
 
         $(window).ready(function() {
             animate.init();
         });
+        $('.header__sound-button').on('click',function(e){
+            console.log($('#background_audio'))
+            if(!mute){
+                $('#background_audio')[0].pause();
+                $('.sound-button--off').css('display','block')
+                $('.sound-button--on').css('display','none')
+            }else{
+                $('#background_audio')[0].play();
+                $('.sound-button--off').css('display','none')
+                $('.sound-button--on').css('display','block')
+            }
+
+            mute = !mute;
+        })
     }
 
     return {
