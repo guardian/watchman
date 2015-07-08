17bb6312a7d368d9ee20bd864ab5b34193f941c2
Sam Morris <mail@sammorrisdesign.co.uk>
2015-07-08 16:20:32 +0100

    Basic navigation

--- src/js/main.js
+++ src/js/main.js
@@ -18,16 +18,16 @@ define([
             animate.init();
             lazyload.init();
         });
-        
-        $('.header__sound-button').on('click',function(e){
+
+        $('.ambient__button').on('click',function(e){
             if(!mute){
                 $('#background_audio')[0].pause();
-                $('.sound-button--off').css('display','block')
-                $('.sound-button--on').css('display','none')
+                $('.ambient__icon--off').css('display','block')
+                $('.ambient__icon--on').css('display','none')
             }else{
                 $('#background_audio')[0].play();
-                $('.sound-button--off').css('display','none')
-                $('.sound-button--on').css('display','block')
+                $('.ambient__icon--off').css('display','none')
+                $('.ambient__icon--on').css('display','block')
             }
             mute = !mute;
         })
