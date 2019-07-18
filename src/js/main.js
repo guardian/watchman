48b934f202067f08c5f1fbbd7b2d456f3fe4e80c
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-07-18 16:30:50 -0400

    restore images

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ window.$ = jquery;
 
 import share from './modules/share.js';
 import audio from './modules/audio.js';
+import scroll from './modules/scroll.js';
 
 share.init();
 audio.init();
+scroll.init();
